0d728907eabe74fdf2dc27beee0b58aae6d1d69b
johnsaturn <john_saturn@hotmail.com>
2015-08-20 20:35:51 -0400

    Firs Draft

---
+++ src/android/CordovaIntentBroadcastListener.java
@@ -0,0 +1,51 @@
+package com.john_saturn.cordova.plugins;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.cordova.CordovaPlugin;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * This class implements a Cordova plugin that allows to execute javascript in the current webView
+ * @author Jose Puertos
+ * 
+ */
+public class CordovaIntentBroadcastListener extends CordovaPlugin {
+
+	public static String ACTION="org.apache.cordova.CordovaPlugin.Intent";
+
+	@Override
+	public void onNewIntent(Intent intent) {
+		//Invoke parent..
+		super.onNewIntent(intent);
+
+		//Match the Action
+		if(ACTION.equals(intent.getAction())){
+
+			Bundle bundle=intent.getExtras();
+			//Get all keys
+			String[] keys=(String[])bundle.keySet().toArray();
+
+			//Iterate over additional values
+			for(int i=0;i<keys.length;i++){
+				String key=keys[i];
+				//Filter keys
+				if(key.startsWith("JS")){
+					String script=bundle.getString(key);
+					try{
+						this.webView.evaluateJavascript(script, null);
+					}catch(Exception ex){
+						Logger.getLogger(this.getClass().getName()).log(Level.WARNING, "Error while processing intent", ex);
+					}
+				}
+
+			}
+
+		}
+
+	}
+
+}
---
+++ www/broadcastListener.js
@@ -0,0 +1,11 @@
+/**
+ * cordova Intent Broadcast Listener
+ * Copyright (c) Jose Puertos 2015
+ *
+ */
+ (function(cordova){
+   
+    // backwards compatibility
+    window.plugins = window.plugins || {};
+
+})(window.PhoneGap || window.Cordova || window.cordova);
